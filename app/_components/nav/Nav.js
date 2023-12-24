f488b34d47e010c71fe3c27e03d9d912ac6972bd
Ben Hajduk <ben.hajduk@luxtripper.co.uk>
2023-12-24 16:12:40 +0000

    added dog

--- app/_components/nav/Nav.js
+++ app/_components/nav/Nav.js
@@ -1,9 +1,11 @@
 import "./nav.css";
+import Image from "next/image";
 
 export default function Nav() {
 	return (
 		<div className="nav">
 			<h1>This is the nav</h1>
+			<Image src="/dog.webp" width={200} height={200} />
 		</div>
 	)
 }
